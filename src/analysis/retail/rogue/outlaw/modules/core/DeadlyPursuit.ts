73bbc3fad69de3211d660faacb9c405eff1d858e
B0uss <7888366+B0uss@users.noreply.github.com>
2026-06-19 09:33:43 +0200

    fix(outlaw): update Deadly Pursuit tracking

--- src/analysis/retail/rogue/outlaw/modules/core/DeadlyPursuit.ts
+++ src/analysis/retail/rogue/outlaw/modules/core/DeadlyPursuit.ts
@@ -1,13 +1,18 @@
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
-import Events, { ApplyBuffEvent, RemoveBuffEvent } from 'parser/core/Events';
+import Events, { ApplyBuffEvent, RefreshBuffEvent, RemoveBuffEvent } from 'parser/core/Events';
 
 import RestlessBlades from './RestlessBlades';
 
-const SPELL = { id: 1259614, name: 'Deadly Pursuit', icon: 'ability_rogue_murderspree' };
-const MULTIPLIER = 3;
+export const DEADLY_PURSUIT = {
+  id: 1259614,
+  name: 'Deadly Pursuit',
+  icon: 'ability_rogue_murderspree',
+};
+
+export const DEADLY_PURSUIT_COOLDOWN_RATE_MULTIPLIER = 3;
 
 export const calculateDeadlyPursuitCooldownReduction = (durationMs: number) =>
-  Math.max(0, durationMs) * MULTIPLIER;
+  Math.max(0, durationMs) * DEADLY_PURSUIT_COOLDOWN_RATE_MULTIPLIER;
 
 class DeadlyPursuit extends Analyzer {
   static dependencies = { restlessBlades: RestlessBlades };
@@ -16,12 +21,22 @@ class DeadlyPursuit extends Analyzer {
 
   constructor(options: Options) {
     super(options);
-    this.addEventListener(Events.applybuff.by(SELECTED_PLAYER).spell(SPELL), this.onApply);
-    this.addEventListener(Events.removebuff.by(SELECTED_PLAYER).spell(SPELL), this.onRemove);
+    this.addEventListener(Events.applybuff.by(SELECTED_PLAYER).spell(DEADLY_PURSUIT), this.onApply);
+    this.addEventListener(
+      Events.refreshbuff.by(SELECTED_PLAYER).spell(DEADLY_PURSUIT),
+      this.onRefresh,
+    );
+    this.addEventListener(Events.removebuff.by(SELECTED_PLAYER).spell(DEADLY_PURSUIT), this.onRemove);
   }
 
   private onApply(event: ApplyBuffEvent) {
     this.appliedAt = event.timestamp;
+  }
+
+  private onRefresh(event: RefreshBuffEvent) {
+    if (this.appliedAt === null) {
+      this.appliedAt = event.timestamp;
+    }
   }
 
   private onRemove(event: RemoveBuffEvent) {
