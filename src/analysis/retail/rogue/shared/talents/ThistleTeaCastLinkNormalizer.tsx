4ba7283db9a20cccf5aabc5240bf825ee6570d3b
B0uss <7888366+B0uss@users.noreply.github.com>
2026-06-19 08:01:21 +0200

    refactor(rogue): expose Thistle Tea links

--- src/analysis/retail/rogue/shared/talents/ThistleTeaCastLinkNormalizer.tsx
+++ src/analysis/retail/rogue/shared/talents/ThistleTeaCastLinkNormalizer.tsx
@@ -1,15 +1,14 @@
 import EventLinkNormalizer, { EventLink } from 'parser/core/EventLinkNormalizer';
 import { CastEvent, EventType, GetRelatedEvent, ResourceChangeEvent } from 'parser/core/Events';
-// import TALENTS from 'common/TALENTS/rogue';
 import SPELLS from 'common/SPELLS/rogue';
 import { Options } from 'parser/core/Module';
 
-const CAST_BUFFER_MS = 50;
+export const THISTLE_TEA_CAST_BUFFER_MS = 50;
 
 const FROM_HARDCAST = 'FromHardcast';
 const RESOURCE_CHANGE = 'ResourceChange';
 
-const EVENT_LINKS: EventLink[] = [
+export const THISTLE_TEA_EVENT_LINKS: EventLink[] = [
   {
     linkRelation: FROM_HARDCAST,
     reverseLinkRelation: RESOURCE_CHANGE,
@@ -17,15 +16,15 @@ const EVENT_LINKS: EventLink[] = [
     linkingEventType: EventType.ResourceChange,
     referencedEventId: SPELLS.THISTLE_TEA.id,
     referencedEventType: EventType.Cast,
-    forwardBufferMs: CAST_BUFFER_MS,
-    backwardBufferMs: CAST_BUFFER_MS,
+    forwardBufferMs: THISTLE_TEA_CAST_BUFFER_MS,
+    backwardBufferMs: THISTLE_TEA_CAST_BUFFER_MS,
     anyTarget: true,
   },
 ];
 
 export default class ThistleTeaCastLinkNormalizer extends EventLinkNormalizer {
   constructor(options: Options) {
-    super(options, EVENT_LINKS);
+    super(options, THISTLE_TEA_EVENT_LINKS);
   }
 }
 
